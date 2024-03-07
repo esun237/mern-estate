a9428e6da7609ec60c87d638de8a0d10a5ea7eed
Yingying Sun <yingying.sun214@gmail.com>
2024-03-07 16:44:45 +1100

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
+    avartar: {
+      type: String,
+      default: "/client/public/images/profile.png",
+    },
   },
   { timestamps: true }
 );
